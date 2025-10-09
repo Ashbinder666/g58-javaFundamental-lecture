501d6941521c7ba238e34057f64f1019afb1578e
Ashbinder666 <ashxela@gmail.com>
2025-10-09 10:02:22 +0200

    Addition Added

--- src/main/java/se/lexicon/Workshop_Calculator.java
+++ src/main/java/se/lexicon/Workshop_Calculator.java
@@ -5,6 +5,17 @@ import java.util.Scanner;
 public class Workshop_Calculator {
     public static void main(String[]args){
 
+Scanner scanner = new Scanner(System.in);
+double fnum, snum, answer;
+
+System.out.println("Enter first number:");
+fnum=scanner.nextDouble();
+
+System.out.println("Enter second number:");
+snum=scanner.nextDouble();
+
+answer = fnum + snum;
+System.out.println(answer);
 
     }
 
