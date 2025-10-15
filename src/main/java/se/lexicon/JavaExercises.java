233ffc80cd019af86de85e2f46e3ae6901f5da99
Ashbinder666 <ashxela@gmail.com>
2025-10-15 09:37:54 +0200

    First code for exercises

---
+++ src/main/java/se/lexicon/JavaExercises.java
@@ -0,0 +1,24 @@
+package se.lexicon;
+
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class JavaExercises {
+    public static void main(String[] args) {
+
+        System.out.println("Hello!");
+        System.out.println("Alex");
+
+        LocalDate currentDate = LocalDate.now();
+       // System.out.println("currentDate = " + currentDate);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter year to see if it's leap year or not");
+
+
+
+
+        
+                
+    }
+}
